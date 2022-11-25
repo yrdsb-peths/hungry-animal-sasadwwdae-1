41773c0fb7bd3e492b9c31a58f6720e31fa35700
asdafwr <105795748+sasadwwdae@users.noreply.github.com>
2022-11-25 13:13:29 -0500

    create apple

--- MyWorld.java
+++ MyWorld.java
@@ -21,7 +21,14 @@ public class MyWorld extends World
         Elephant elephant = new Elephant();
         addObject(elephant,300,300);
         
+        createApple();
+    }
+    
+    public void createApple()
+    {
         Apple apple = new Apple();
-        addObject(apple,300,0);
+        int x = Greenfoot.getRandomNumber(600);
+        int y = (0);
+        addObject(apple,x,y);
     }
 }
