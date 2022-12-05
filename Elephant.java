13f99dca8bba396d1e227ef48c1269a81db81d2f
asdafwr <105795748+sasadwwdae@users.noreply.github.com>
2022-12-05 12:54:11 -0500

    change the speed of swing the trunk

--- Elephant.java
+++ Elephant.java
@@ -17,6 +17,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     
     public Elephant()
     {
@@ -33,12 +34,19 @@ public class Elephant extends Actor
             idleLeft[i].scale(80,80);
         }
         
+        animationTimer.mark();
+        
         setImage(idleRight[0]);
     }
     
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 20)
+        {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
